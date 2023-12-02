d37b50568f45d1892f6a73b0d9c6b870c92c535c
Arthur Oliveira <arthurolvmorais@gmail.com>
2023-12-02 10:19:11 -0300

    refactor(tests): remove test suites to use hooks in future
    Change tests, before it was grouping tests for similar groups in
    suites, but in future it will be necessary to use the hooks afterEach
    and beforeEach and this "each" means suites and not individual tests.
    
    In the hooks it will be the connection and the desconnection with
    MongoDB

--- src/tests.mjs
+++ src/tests.mjs
@@ -16,79 +16,66 @@ test('Should return Hello World for / endpoint', async (t) => {
   app.close();
 });
 
-test('Tests for registers', async (t) => {
-  await t.test('Should save a new entrance for /batidas endpoint', async () => {
-    const response = await request(app)
-      .post('/batidas')
-      .send({ momento: '2023-11-29T08:00:00' });
+test('Should save a new entrance for /batidas endpoint', async () => {
+  const response = await request(app)
+    .post('/batidas')
+    .send({ momento: '2023-11-29T08:00:00' });
 
-    const EXPECTED_STATUS_CODE = 200;
-    const EXPECTED_RESPONSE = { dia: '2023-11-29', pontos: ['08:00:00'] };
-    const EXPECTED_RESPONSE_TYPE = 'application/json';
+  const EXPECTED_STATUS_CODE = 200;
+  const EXPECTED_RESPONSE = { dia: '2023-11-29', pontos: ['08:00:00'] };
+  const EXPECTED_RESPONSE_TYPE = 'application/json';
 
-    assert.strictEqual(response.type, EXPECTED_RESPONSE_TYPE);
-    assert.strictEqual(response.status, EXPECTED_STATUS_CODE);
-    assert.deepEqual(response.body, EXPECTED_RESPONSE);
+  assert.strictEqual(response.type, EXPECTED_RESPONSE_TYPE);
+  assert.strictEqual(response.status, EXPECTED_STATUS_CODE);
+  assert.deepEqual(response.body, EXPECTED_RESPONSE);
 
-    app.close();
-  });
-
-  await t.test(
-    'Should return a Bad Request error if not receive "momento" parameter',
-    async () => {
-      const response = await request(app).post('/batidas');
-
-      const EXPECTED_STATUS_CODE = 400;
-      const EXPECTED_RESPONSE_TYPE = 'application/json';
-      const EXPECTED_RESPONSE = {
-        mensagem: messages.REGISTER.INVALID_PARAMETER,
-      };
-
-      assert.strictEqual(response.type, EXPECTED_RESPONSE_TYPE);
-      assert.strictEqual(response.status, EXPECTED_STATUS_CODE);
-      assert.deepEqual(response.body, EXPECTED_RESPONSE);
-
-      app.close();
-    },
-  );
-
-  await t.test(
-    'Should return a Bad Request error if "momento" parameter is not a valid date',
-    async () => {
-      const response = await request(app)
-        .post('/batidas')
-        .send({ momento: 'dasdsadsa' });
-
-      const EXPECTED_STATUS_CODE = 400;
-      const EXPECTED_RESPONSE_TYPE = 'application/json';
-      const EXPECTED_RESPONSE = {
-        mensagem: messages.REGISTER.INVALID_PARAMETER_TYPE,
-      };
-
-      assert.strictEqual(response.type, EXPECTED_RESPONSE_TYPE);
-      assert.strictEqual(response.status, EXPECTED_STATUS_CODE);
-      assert.deepEqual(response.body, EXPECTED_RESPONSE);
-
-      app.close();
-    },
-  );
+  app.close();
 });
 
-test('Tests for reports', async (t) => {
-  await t.test(
-    'Should return TO DO for /folhas-de-ponto/:mes endpoint',
-    async () => {
-      const response = await request(app).get('/folhas-de-ponto/22222');
+test('Should return a Bad Request error if not receive "momento" parameter', async () => {
+  const response = await request(app).post('/batidas');
 
-      const EXPECTED_STATUS_CODE = 200;
-      const EXPECTED_RESPONSE = { message: 'TO DO' };
-      const EXPECTED_RESPONSE_TYPE = 'application/json';
+  const EXPECTED_STATUS_CODE = 400;
+  const EXPECTED_RESPONSE_TYPE = 'application/json';
+  const EXPECTED_RESPONSE = {
+    mensagem: messages.REGISTER.INVALID_PARAMETER,
+  };
 
-      assert.strictEqual(response.type, EXPECTED_RESPONSE_TYPE);
-      assert.strictEqual(response.status, EXPECTED_STATUS_CODE);
-      assert.deepEqual(response.body, EXPECTED_RESPONSE);
+  assert.strictEqual(response.type, EXPECTED_RESPONSE_TYPE);
+  assert.strictEqual(response.status, EXPECTED_STATUS_CODE);
+  assert.deepEqual(response.body, EXPECTED_RESPONSE);
 
-      app.close();
-    },
-  );
+  app.close();
+});
+
+test('Should return a Bad Request error if "momento" parameter is not a valid date', async () => {
+  const response = await request(app)
+    .post('/batidas')
+    .send({ momento: 'dasdsadsa' });
+
+  const EXPECTED_STATUS_CODE = 400;
+  const EXPECTED_RESPONSE_TYPE = 'application/json';
+  const EXPECTED_RESPONSE = {
+    mensagem: messages.REGISTER.INVALID_PARAMETER_TYPE,
+  };
+
+  assert.strictEqual(response.type, EXPECTED_RESPONSE_TYPE);
+  assert.strictEqual(response.status, EXPECTED_STATUS_CODE);
+  assert.deepEqual(response.body, EXPECTED_RESPONSE);
+
+  app.close();
+});
+
+test('Should return TO DO for /folhas-de-ponto/:mes endpoint', async () => {
+  const response = await request(app).get('/folhas-de-ponto/22222');
+
+  const EXPECTED_STATUS_CODE = 200;
+  const EXPECTED_RESPONSE = { message: 'TO DO' };
+  const EXPECTED_RESPONSE_TYPE = 'application/json';
+
+  assert.strictEqual(response.type, EXPECTED_RESPONSE_TYPE);
+  assert.strictEqual(response.status, EXPECTED_STATUS_CODE);
+  assert.deepEqual(response.body, EXPECTED_RESPONSE);
+
+  app.close();
 });
